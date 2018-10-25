b8a73c133e6cddddda39fac281e756d402c908d0
Jasen Hall <jasenmh@yahoo.com>
2018-10-24 19:13:42 -0700

    jmh - improve formatting

--- lotto.rb
+++ lotto.rb
@@ -1,45 +1,45 @@
 #!/usr/bin/env ruby
 
 def get_number
-        printf 'enter the ticket number: '
-        number = gets.chomp
-        number.split
+	printf 'enter the ticket number: '
+	number = gets.chomp
+	number.split
 end
 
 def format_number(ticket_number)
-        tn = ticket_number
-        # "#{tn[0]} #{tn[1]} #{tn[2]} #{tn[3]} #{tn[4]} x#{tn[5]}"
+	tn = ticket_number
+	# "#{tn[0]} #{tn[1]} #{tn[2]} #{tn[3]} #{tn[4]} x#{tn[5]}"
+	
+	num = ""
+	tn[0..-2].each do |digit|
+		num += digit.length > 1 ? " "+digit : " 0"+digit
+	end
+	num += tn[-1].length > 1 ? " M"+tn[-1] : " M0"+tn[-1]
 
-        num = ""
-        tn[0..-2].each do |digit|
-                num += digit.length > 1 ? " "+digit : " 0"+digit
-        end
-        num += tn[-1].length > 1 ? " M"+tn[-1] : " M0"+tn[-1]
-
-        num
+	num
 end
 
 def compare_numbers(ticket_number, winning_number)
-        matching = 0
-        multiplier = false
+	matching = 0
+	multiplier = false
 
-        winning_number[0..-2].each do |win_digit|
-                ticket_number[0..-2].each do |test_digit|
-                        if win_digit == test_digit
-                                matching += 1
-                        end
-                end
-        end
+	winning_number[0..-2].each do |win_digit|
+		ticket_number[0..-2].each do |test_digit|
+			if win_digit == test_digit
+				matching += 1 
+			end
+		end
+	end
 
-        multiplier = true if winning_number[-1] == ticket_number[-1]
+	multiplier = true if winning_number[-1] == ticket_number[-1]
 
-        [ matching, multiplier ]
+	[ matching, multiplier ]
 end
 
 def result_is_winner(result)
-        return true if ( result[0] >= 3 or result[1] )
+	return true if ( result[0] >= 3 or result[1] )
 
-        false
+	false
 end
 
 puts "winning number first"
@@ -48,15 +48,15 @@ winning_number = get_number
 tickets = []
 puts "enter ticket numbers (enter to stop)"
 while (ticket_number = get_number).length > 0
-        tickets += [ ticket_number ]
+	tickets += [ ticket_number ]
 end
 
 printf("Winning Number: %s\n", format_number(winning_number))
-puts " Win | Ticket Number  | Matches | Multiplier "
-
+puts " Win |   Ticket Number    | Matches | Multiplier "
+	
 tickets.each do |ticket_number|
-        result = compare_numbers(ticket_number, winning_number)
-        winner = result_is_winner(result) ? 'Y' : 'N'
-        multiplier = result[1] ? 'Y' : 'N'
-        puts "  #{winner}  | #{format_number(ticket_number)} |    #{result[0]}    |       #{multiplier}"
+	result = compare_numbers(ticket_number, winning_number)
+	winner = result_is_winner(result) ? 'Y' : 'N'
+	multiplier = result[1] ? 'Y' : 'N'
+	puts "  #{winner}  |#{format_number(ticket_number)} |    #{result[0]}    |      #{multiplier}"
 end
